dc6c95464ac516ea09395d4657b4d353d777cc6c
angelaRG05 <4048525@alu.murciaeduca.es>
2025-12-09 17:56:34 +0100

    arreglo script

--- Assets/Script/TestScript.cs
+++ Assets/Script/TestScript.cs
@@ -1,8 +1,15 @@
 using UnityEngine;
+using Unity.Netcode;
 
-public class TestScript : MonoBehaviour
+public class TestScript : NetworkBehaviour
 {
     [SerializeField] private InputReader inputReader;
+    [SerializeField] private int speed;
+
+    public int move = 5;
+    public int rotation = 150;
+    public Transform turret;
+
     private void Start()
     {
         inputReader.MoveEvent += HandleMove;
@@ -18,9 +25,41 @@ public class TestScript : MonoBehaviour
     private void HandleMove(Vector2 movement)
     {
         Debug.Log("Movimiento: " + movement);
+        //if (!IsOwner) return;
+        //Vector3 dir = new Vector3(movement.x, movement.y, 0f).normalized;
+        //transform.position += dir speed * Time.deltaTime;
     }
+
     private void HandlePrimaryFire(bool isFiring)
     {
         Debug.Log("Disparando: " + isFiring);
+    }
+
+    void Update()
+    {
+        if (!IsOwner) return;
+
+        float movement = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.forward * movement * -rotation * Time.deltaTime);
+
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            transform.Translate(Vector3.up * move * Time.deltaTime);
+        }
+
+        else if (Input.GetAxis("Vertical") < 0)
+        {
+            transform.Translate(Vector3.down * move * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.O))
+        {
+            turret.transform.Rotate(Vector3.forward * rotation * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.P))
+        {
+            turret.transform.Rotate(Vector3.forward * -rotation * Time.deltaTime);
+        }
     }
 }
